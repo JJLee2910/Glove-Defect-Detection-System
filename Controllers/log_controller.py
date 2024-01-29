2c4c734a9bfb3cc82e826f46d4fda0d71a42b986
JJ <jangjhinl@yahoo.com>
2024-01-29 23:20:26 +0800

    updated log controller on its button functions

--- Controllers/log_controller.py
+++ Controllers/log_controller.py
@@ -1,6 +1,8 @@
 from Controllers.main_dashboard_controller import *
 from UI_Design.log import *
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QStackedWidget
+from enums import Pages
+
 class LogController(QMainWindow):
 
     def __init__(self, router):
@@ -14,7 +16,20 @@ class LogController(QMainWindow):
 
         #
         self.ui.DashboardButton.clicked.connect(self.go_dashboard)
+        self.ui.ManualinspectionBox.currentIndexChanged.connect(self.switch_manual_inspection_screen)
+        self.ui.SimulationButton.clicked.connect(self.switch_simulation_screen)
 
     def go_dashboard(self):
-        print("0")
-        self.router.setCurrentIndex(0)
+        self.router.setCurrentIndex(Pages.DASHBOARD.value)
+
+    def switch_manual_inspection_screen(self):
+        selected_option = self.ui.ManualinspectionBox.currentText()
+        self.ui.ManualinspectionBox.setCurrentIndex(0)
+
+        if selected_option == "Latex Glove":
+            print("1")
+            self.router.setCurrentIndex(Pages.MANUAL_INSPECTION.value)
+
+    def switch_simulation_screen(self):
+        if self.ui.SimulationButton.isChecked() == False:
+            print("3")

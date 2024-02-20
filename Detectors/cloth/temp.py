54c65e2c571817ae55756dcf3a540027c50d8ff0
JJLee2910 <jangjhinL02@gmail.com>
2024-02-20 13:45:54 +0800

    update temp.py in cloth detectors

--- Detectors/cloth/temp.py
+++ Detectors/cloth/temp.py
@@ -6,7 +6,7 @@ from tools.color_based_binarizer import ColorBasedBinarizer
 class StainDetector(Detector):
     def __init__(self, img) -> None:
         self.img = img
-        # self.img = cv2.resize(img, None, fx=0.2, fy=0.2
+        # self.img = cv2.resize(img, None, fx=0.2, fy=0.2)
     def detect(self):
         # binarize the images
         result = ColorBasedBinarizer.apply(self.img,0.0, 0.2, 0.1)

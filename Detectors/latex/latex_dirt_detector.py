329cd8694d534d8c4fae672d2028daef62d53b27
JJLee2910 <jangjhinL02@gmail.com>
2024-03-05 20:13:16 +0800

    updated the dirt detection for latex gloves

--- Detectors/latex/latex_dirt_detector.py
+++ Detectors/latex/latex_dirt_detector.py
@@ -2,19 +2,21 @@ import cv2
 import numpy as np
 from Detectors.base import Detector
 from tools.color_based_binarizer import ColorBasedBinarizer
-from tools.missing_finger_checker import MissingFingerChecker
+from tools.stain_finder import StainFinder
+from tools.resizer import Resizer
 
-class DirtDetectors(Detector):
+class DirtDetector(Detector):
     def __init__(self, img) -> None:
         self.img = img
-        self.img = cv2.resize(img, None, fx=0.2, fy=0.2)
 
     def detect(self):
-        mask = ColorBasedBinarizer.apply(self.img,0.1, 0.2, 0.3)
-        result = MissingFingerChecker.apply(self.img,mask)
-        
-        if (result is not None):
-            cv2.imshow("Combined", result)
-        else:
-            cv2.imshow(self.img)
+        # binarize the images
+        result = ColorBasedBinarizer.apply(self.img, 0.1, 0.2, 0.3)
+
+        result = StainFinder().apply(result)
+
+        result = Resizer.apply(result)
+
+        cv2.imshow("Result", result)
         cv2.waitKey(0)
+        cv2.destroyAllWindows()

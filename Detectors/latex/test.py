3cd39a43fae68cc92e347a497a4453b03b4353d7
JJLee2910 <jangjhinL02@gmail.com>
2024-03-07 19:00:30 +0800

    updated tearing for latex gloves

--- Detectors/latex/test.py
+++ Detectors/latex/test.py
@@ -4,7 +4,7 @@ import numpy as np
 # Fixed size for all the displayed screen
 fixed_size = (550, 550)
 
-image_path = 'C:\\Users\\JJ\\OneDrive\\Desktop\\Glove-Defect-Detection-System\\Images\\Latex\\tear.jpg'
+image_path = 'C:\\Users\\JJ\\OneDrive\\Desktop\\Glove-Defect-Detection-System\\Images\\Latex\\tear_5.jpg'
 frame = cv2.imread(image_path)
 
 # Resize the image
@@ -24,7 +24,7 @@ blurred_frame = cv2.medianBlur(mask, 9)
 cv2.imshow('bf', blurred_frame)
 
 # Define the structuring element (kernel) for erosion
-kernel = np.ones((5, 5), np.uint8)
+kernel = np.ones((3, 3), np.uint8)
 
 # Perform erosion
 eroded_frame = cv2.erode(blurred_frame, kernel)
@@ -32,15 +32,8 @@ cv2.imshow('ef', eroded_frame)
 
 # Find Contours
 # findContours alters the image to show only the glove (blue color)
-contours, hierarchy = cv2.findContours(eroded_frame.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 cv2.drawContours(eroded_frame, contours, -1, (0, 255, 0), 2)
-# print('hierarchy',hierarchy, '\n')
-# print('contours', contours)
-
-# Draw convex hulls on the gloves
-# for contour in contours:
-#     hull = cv2.convexHull(contour)
-#     cv2.drawContours(frame, [hull], -1, (255, 0, 0), 2)
 
 # Detect the defect within the glove
 internal_cnt = [contours[i] for i in range(len(contours)) if hierarchy[0][i][3] >= 0]
@@ -64,7 +57,7 @@ if len(contours) > 0:
             # Check defect size
             area = cv2.contourArea(i)
             print('area: ',area, '\n')
-            if area > 70:
+            if area > 40:
                 xd, yd, wd, hd = cv2.boundingRect(i)
                 # Draw rectangle for defect
                 cv2.rectangle(frame, (xd, yd), (xd + wd, yd + hd), (0, 0, 255), 1)
@@ -74,7 +67,7 @@ if len(contours) > 0:
                 print(hd, '\n')
 
                 # Label the defect
-                if area > 500:
+                if area > 400:
                     # Defect Type: Tearing
                     frame = cv2.putText(frame, 'Tearing', (xd, yd - 5), cv2.FONT_HERSHEY_SIMPLEX,
                                         0.5, (0, 0, 255), 1, cv2.LINE_AA)

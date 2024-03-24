800614e5466a4cf8b0190f4094706fad91ba58ab
andy <yuseifudo888@yahoo.com>
2024-03-24 16:10:27 +0800

    plastic

--- Detectors/plastic/plastic_dirty.py
+++ Detectors/plastic/plastic_dirty.py
@@ -29,16 +29,16 @@ class DirtyPlasticGlovesDetector(Detector):
         # Convert the gloves image to grayscale
         grey = cv2.cvtColor(gloves_img, cv2.COLOR_BGR2GRAY)
 
-        # Apply Gaussian blur to reduce noise and enhance dot detection
+        # Apply Gaussian blur to reduce noise and enhance dirty gloves detection
         blurred = cv2.GaussianBlur(grey, (11, 11), 0)
 
-        # Threshold the blurred image to create a binary mask for black dots
+        # Threshold the blurred image to create a binary mask for dirty gloves
         _, thresh = cv2.threshold(blurred, 30, 255, cv2.THRESH_BINARY_INV)
 
         # Find contours in the binary image
         contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        # Draw circles around the detected black dots on the gloves image
+        # Draw circles around the detected dirty gloves image
         result_img = self.img.copy()
         for contour in contours:
             # Calculate the minimum enclosing circle for each contour
@@ -49,7 +49,7 @@ class DirtyPlasticGlovesDetector(Detector):
             # Draw the circle with a larger radius
             cv2.circle(result_img, center, radius + 10, (0, 0, 255), 5)  # Increase the radius by 10
 
-        # Add text based on dots detection
+        # Add text based on dirty gloves detection
         if len(contours) > 0:
             text = "Dirty gloves detected"
             text_color = (0, 0, 255)  # Red color for dirty gloves
@@ -64,6 +64,6 @@ class DirtyPlasticGlovesDetector(Detector):
         result_img = Resizer.apply(result_img)
 
         # Display the result image with text and circles
-        cv2.imshow("Gloves and Dots Detection Result", result_img)
+        cv2.imshow("Dirty Gloves Detection Result", result_img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
